9dbc3bc2068a76a8cde01a5dd139b91781abeab7
berarayappan <berarayappan@gmail.com>
2026-08-07 20:28:00 +0530

    Time: 0 ms (100.00%) | Memory: 19.3 MB (28.32%) - LeetSync

--- 48-rotate-image/rotate-image.py
+++ 48-rotate-image/rotate-image.py
@@ -5,12 +5,13 @@ class Solution:
         """
 
         n= len(matrix)
-        temp = [[0] * n for _ in range(n)]
+        for i in range(n):
+            for j in range(i+1,n):
+                matrix[i][j],matrix[j][i] = matrix[j][i] ,matrix[i][j]
 
         for i in range(n):
-            for j in range(n):
-                temp[j][n - 1 - i] = matrix[i][j]
-
-        for i in range(n):
-            for j in range(n):
-                matrix[i][j] = temp[i][j]
+            left,right=0 ,n-1
+            while left < right:
+                matrix[i][left],matrix[i][right] = matrix[i][right],matrix[i][left]
+                left += 1
+                right -= 1
